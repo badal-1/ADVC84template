194ae82ecbb1fd449cd1804e08c67d8985a1ce12
SnehalbSawant <69292647+SnehalbSawant@users.noreply.github.com>
2021-04-28 15:45:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
+// Create a reference for the canvas
 
 img_width = 300;
 img_height = 100;
@@ -20,7 +19,7 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-window.addEventListener("keydown", my_keydown);
+//Write a code to grab the key-pressed event
 
 function my_keydown(e)
 {
@@ -28,29 +27,7 @@ function my_keydown(e)
 	console.log(keyPressed);
 	
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
-		{
-			aplhabetkey();
-			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
-			console.log("alphabet key");
-		}
-		else if(keyPressed >=48 && keyPressed<=57)
-		{
-			numberkey();
-			document.getElementById("d1").innerHTML="You pressed Number Key";
-			console.log("Number key");
-		}
-		else if(keyPressed >=37 && keyPressed<=40)
-		{
-			arrowkey();
-			document.getElementById("d1").innerHTML="You pressed Arrow Key";
-			console.log("Arrow Key");
-		}
-		else if((keyPressed ==17)|| (keyPressed ==18 || keyPressed ==27))
-		{
-			specialkey();
-			document.getElementById("d1").innerHTML="You pressed ctrl/esc/alt";
-			console.log("special key");
-		}
+		//write a code to check the type of key pressed
 	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
@@ -59,23 +36,19 @@ function my_keydown(e)
 
 function aplhabetkey()
 {
-		img_image="Alpkey.png";
-	add();
+	//upload respective image with the message. 
+
 }
 function numberkey()
 {
-	img_image="numkey.png";
-	add();
+	
 }
 function arrowkey()
 {
-	img_image="Arrkey.png";
-	add();
 }
 function specialkey()
 {
-	img_image="spkey.png";
-	add();
+	
 }
 function otherkey()
 {
